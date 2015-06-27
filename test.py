760352b20be0823194ad50c93b973dee43dab812
HuaizhengZhang <mishuzhang33@126.com>
2015-06-27 15:33:27 +0800

    量比数据测试

--- test.py
+++ test.py
@@ -7,10 +7,37 @@ Copyright (c) 2015 zhzHNN. All rights reserved.
 
 """
 import stock_get as sg
-print(sg.get_fq_day_data('002024'))
+import numpy as np
+#print(sg.get_fq_day_data('002024'))
 
 #print(sg.get_realtime_quotes('000581'))
-#sg.get_fq_day_data('002024').to_csv('002024复权数据.csv',encoding='utf8')
+#sg.get_fq_day_data('000009').to_csv('000009复权数据.csv',encoding='utf8')
+
+
+"""Volumn = np.loadtxt('000009复权数据.csv',delimiter=',' , skiprows=1, usecols=(5,), unpack=True)
+for x in xrange(0,481):
+	Volumn[x] = Volumn[x+534]
+for x in xrange(0,476):
+	sum = (Volumn[x+1] + Volumn[x+2] + Volumn[x+3] + Volumn[x+4] + Volumn[x+5]) / 5
+	Volumn[x] = Volumn[x] / sum
+	print Volumn[x]
+np.savetxt('000009train.csv',Volumn,fmt='%s')"""
+
+Volumn = np.loadtxt('000009复权数据.csv',delimiter=',' , skiprows=1, usecols=(5,), unpack=True)
+for x in xrange(0,239):
+	Volumn[x] = Volumn[x+300]
+for x in xrange(0,234):
+	sum = (Volumn[x+1] + Volumn[x+2] + Volumn[x+3] + Volumn[x+4] + Volumn[x+5]) / 5
+	Volumn[x] = Volumn[x] / sum
+	print Volumn[x]
+np.savetxt('000009test.csv',Volumn,fmt='%s')
+
+
+
+"""for x in xrange(0,3563):
+	if x+5 < 3400:
+		lb[x] =Volumn[x] / ((Volumn[x+1] + Volumn[x+2]+ Volumn[x+3]+ Volumn[x+4]+ Volumn[x+5])/5)
+print lb"""
 #temp = sg.get_sharebonus_1_data('002024')
 #temp.to_csv('0020242分红（新浪网）.csv',encoding='utf8')
 #print(temp)
